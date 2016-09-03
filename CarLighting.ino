62c760ca1dd8c47045a22182388caf4de0bbd9d9
JaredF <jfailor@cfu.net>
2016-09-03 16:48:05 -0400

    Removed OBD until supported hardware arrives. Fixed name indicies for display. Added setting saving and loading.

--- CarLighting.ino
+++ CarLighting.ino
@@ -8,29 +8,39 @@
 #include "ColorInterpolater.h"
 #include <Adafruit_NeoPixel.h>
 #include <LiquidCrystal.h>
+#include <EEPROM.h>
 
 static const int LED_COUNT = 8;
-static const int SETTING_COUNT = 3;
+static const int SETTING_COUNT = 8;
 static const int BUTTON_COUNT = 6;
 static const int BUTTON_TOLERANCE = 2;
 
 bool lightsOn = true;
+bool useObd = false;
 Adafruit_NeoPixel neo = Adafruit_NeoPixel(8, 6, NEO_GRB + NEO_KHZ800);
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 COBD *obd = new COBD();
 GaugeColor* gaugeColors[SETTING_COUNT] = { new SolidColor(LEDColor(0, 0, 255), "Blue"),
 											new SolidColor(LEDColor(255, 0, 0), "Red"),
-											new EngineColor(obd, PID_RPM, "RPM", LEDColor(255, 255, 255), LEDColor(255, 50, 50), 0, 7000)};
+											new SolidColor(LEDColor(255, 255, 255), "White"),
+											new SolidColor(LEDColor(255, 255, 155), "Real White"),
+											new SolidColor(LEDColor(0, 255, 0), "Green"),
+											new SolidColor(LEDColor(160, 0, 255), "Purple"),
+											new SolidColor(LEDColor(255, 100, 0), "Orange"),
+											new SolidColor(LEDColor(255, 255, 0), "Yellow") };
+											//new EngineColor(obd, PID_RPM, "RPM", LEDColor(255, 255, 255), LEDColor(255, 50, 50), 0, 7000)};
 
-int lightSettingIndicies[LED_COUNT] = {1, 0, 1, 0, 0, 0, 0, 0};
+uint8_t lightSettingIndicies[LED_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 int buttonValues[BUTTON_COUNT] = { 959, 985, 839, 512, 696, 1009 };
 AnalogButtonsDown buttons = AnalogButtonsDown(0, BUTTON_COUNT, buttonValues, BUTTON_TOLERANCE);
 
-String gaugeNames[BUTTON_COUNT] = { "Engine Temp", "RPM", "Speed", "Fuel Level", "Trip", "Info" };
+String gaugeNames[BUTTON_COUNT] = { "Fuel Level", "RPM", "Trip", "Info", "Speed", "Engine Temp" };
 
 void setup()
 {
+	loadSettings();
+
 	lcd.begin(16, 2);
 	lcd.print("Welcome");
 
@@ -41,9 +51,6 @@ void setup()
 		}
 		neo.show();
 	}
-
-	obd->begin();
-	while (!obd->init());
 }
 
 void loop()
@@ -52,6 +59,7 @@ void loop()
 	if (buttonPressed != -1) {
 		incrementBasedOnGroup(buttonPressed);
 		setLCD(buttonPressed);
+		saveSettings();
 	}
 
 	if (lightsOn) {
@@ -66,8 +74,27 @@ void setLCD(int buttonPressed) {
 	lcd.clear();
 	lcd.setCursor(0, 0);
 	lcd.print(gaugeNames[buttonPressed]);
-	lcd.setCursor(0, 1);
-	lcd.print(gaugeColors[lightSettingIndicies[buttonPressed]]->_name);
+	lcd.setCursor(0, 1); 
+	switch (buttonPressed) {
+	case 0:
+		lcd.print(gaugeColors[lightSettingIndicies[0]]->_name);
+		break;
+	case 1:
+		lcd.print(gaugeColors[lightSettingIndicies[1]]->_name);
+		break;
+	case 2:
+		lcd.print(gaugeColors[lightSettingIndicies[6]]->_name);
+		break;
+	case 3:
+		lcd.print(gaugeColors[lightSettingIndicies[4]]->_name);
+		break;
+	case 4:
+		lcd.print(gaugeColors[lightSettingIndicies[3]]->_name);
+		break;
+	case 5:
+		lcd.print(gaugeColors[lightSettingIndicies[2]]->_name);
+		break;
+	}
 }
 
 void incrementBasedOnGroup(int buttonPressed) { //LEDs 4 and 5 and LEDs 6 and 7 are paired up for a single gauge
@@ -99,5 +126,20 @@ void incrementSetting(int index) {
 	lightSettingIndicies[index]++;
 	if (lightSettingIndicies[index] >= SETTING_COUNT) {
 		lightSettingIndicies[index] = 0;
+	}
+}
+
+void loadSettings() {
+	for (int i = 0; i < LED_COUNT; i++) {
+		lightSettingIndicies[i] = EEPROM.read(i);
+		if (lightSettingIndicies[i] >= SETTING_COUNT) {
+			lightSettingIndicies[i] = 0;
+		}
+	}
+}
+
+void saveSettings() {
+	for (int i = 0; i < LED_COUNT; i++) {
+		EEPROM.write(i, lightSettingIndicies[i]);
 	}
 }
